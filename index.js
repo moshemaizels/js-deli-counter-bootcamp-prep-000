60c764b931e52e1c5f5b7fc0094f2502743a70ed
Moshe Maizels <moshemaizels@gmail.com>
2017-08-20 15:45:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ function takeANumber(currentLine, name){
 function newServing(currentLine) {
   if (currentLine.length === 0) {
     return "There is nobody waiting to be served!"
+  } else {
+    var currentCust = currentLine[0];
+    currentLine.
   }
 
 }
