d1ebe211b58672470ff545d3abe478b05f743158
Moshe Maizels <moshemaizels@gmail.com>
2017-08-20 15:49:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,13 @@ function nowServing(currentLine) {
   }
 
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return "The line is currently empty."
+  } else {
+    for (var i = 0; i < line.length; i++) {
+      
+    }
+  }
+}
