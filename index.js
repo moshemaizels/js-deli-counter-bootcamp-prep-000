beb9ca7a64c9dcc6317c1b73debf502b27ae8f5d
Moshe Maizels <moshemaizels@gmail.com>
2017-08-20 15:53:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    lineReadout = "The line is currently"
+    lineReadout = "The line is currently:"
     for (var i = 0; i < line.length; i++) {
-      
+      lineReadout = lineReadout + ` ${i+1}. ${line[i]},`
     }
   }
 }
